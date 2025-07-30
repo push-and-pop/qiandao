d190d464ce75c59fff09178045da366d26a7547f
push-and-pop <1916005008@qq.com>
2025-07-30 08:18:00 +0800

    fix

--- soushuba.py
+++ soushuba.py
@@ -121,7 +121,7 @@ class SouShuBaClient:
         return hcredit_2
 
     def space_form_hash(self):
-        rst = self.session.get(f'https://{self.hostname}/home.php',verify = False).text
+        rst = self.session.get(f'https://{self.hostname}',verify = False).text
         formhash = re.search(r'<input type="hidden" name="formhash" value="(.+?)" />', rst).group(1)
         return formhash
 
